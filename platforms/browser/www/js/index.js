999f7d779b27424702f11e44e69811743b98311f
anand100b <57651518+anand100b@users.noreply.github.com>
2020-05-04 02:18:14 +1000

    todo list app with add and delete function

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,125 @@
+var app = {
+    // Application Constructor
+    initialize: function () {
+        document.addEventListener('deviceready', this.devicereadyListener.bind(this), false);
+
+        var localStorage = window.localStorage; 
+        $("#addTask").click(this.newListListner);  
+        $("#delete").click(this.deleteListner);
+        $("#delete_completed").click(this.deleteListListner); 
+       
+    },
+
+   
+   
+    deleteListListner: function (e) {
+        var target = $(e.target);
+        if (target.is(":checked")) {
+           
+            document.getElementById("#delete").disabled = false;
+            localStorage.setItem("cbo", 1);
+        } else {
+            document.getElementById("#delete").disabled = true;
+            localStorage.removeItem("#cbo");
+        }
+        var list = $("#list").html();
+        localStorage.setItem("list", list);
+    },
+
+    newListListner: function () {
+
+        var userList = $('#inputTask').val();
+        if (userList == "") {
+            onclick("#addTask");
+            alert("enter the text first");
+            return;
+        }
+        else {
+         
+            $('#inputTask').val('');
+            var newitem = '<table class="table" border="1" border-collapse="collapse"> <tr> <td><input id="cbo" type="checkbox"  name="checkbox-name"/> '
+                                                          + userList +
+                '<input type="button" value="Delete" disabled="fasle" id="delete" class="w3-btn w3-black" style="float:right"/> </td></tr></table>';
+            $('#list').append(newitem);
+
+            var list = $("#list").html();
+            localStorage.setItem('list', list);
+       
+            $('#addTask').addClass("inactivePage");
+           
+            $('#listPage').removeClass("inactivePage");
+        }
+    },
+
+    deleteListner: function () {
+        var deleterows = $(".table input:checked").parents("tr").remove();
+        parents.remove();
+
+        var list = $("#list").html();
+        localStorage.setItem("list", list);
+        return false;
+    },
+
+    
+
+    comboListner: function () {
+
+        $('#cbo').val($(this).is(':checked'));
+        if (document.getElementById($('#cbo')).checked) {  
+            document.getElementById("delete").disabled = false;    
+        } else {
+            document.getElementById("delete").disabled = true;
+        }
+       
+    },
+
+    devicereadyListener: function () {
+       
+        $('#listPage').removeClass("inactivePage");
+        if (localStorage.getItem('list')) {
+            $('#list').html(localStorage.getItem('list'));
+        }
+        $('#delete').click(function () {
+           
+           
+            var parent = $(this).parent();
+            parent.remove();
+
+            var list = $("#list").html();
+        localStorage.setItem("list", list);
+        });
+
+        $('#cbo').click(function (e) {
+            var target = $(e.target);
+            if (target.is(":checked")==true) {
+                document.getElementById("delete").disabled = false;
+                localStorage.setItem("cbo", 1);
+            } else {
+                document.getElementById("delete").disabled = true;
+                localStorage.removeItem("cbo");
+            }
+        });
+
+        $(document).on('change', "input[name='checkbox-name']", function (e) {
+            if ($(this).prop('checked')==true) {
+              $(this).attr('checked', 'checked');
+              
+            } else {
+              $(this).removeAttr('checked');
+            }
+            var list = $("#list").html();
+            localStorage.setItem("list", list);
+          });
+
+    },
+
+};
+
+app.initialize();
+
+    
+
+       
+
+
+
---
+++ platforms/ios/www/js/index.js
@@ -0,0 +1,48 @@
+
+
+    
+    //         var addButton = document.getElementById('addButton');
+    //         var todoInput = documnet.getElementById('todoInput');
+    //         var list = document.getElementById('list');
+
+    //         addButton.addEventListener('click', function(){
+    //             var todo = todoInput.value;
+    //             var item = document.createElement('DIV');
+    //             item.classList.add('item');
+    //             var item_text = document.createElement('DIV');
+    //             item_text.classList.add('item-text');
+    //             item_text.textContent = todo;
+
+    //             var edit_input = document.createElement('INPUT');
+    //             edit_input.classList.add('edit-input');
+    //             edit_input.classList.add('hide');
+    //             edit_input.name='edit-input';
+    //             edit_input.type='text';
+    //             edit_input.value=todo;
+
+    //             var deleteButton = document.createElement('Button');
+    //             deleteButton.classList.add('action-button');
+    //             deleteButton.classList.add('deleteButton');
+    //             deleteButton.textContent='Delete';
+    
+    //             deleteButton.addEventListener('click',function(){
+    //                 item.parentNode.removeChild(item);
+    //             });
+    
+                
+    //             actionButton.append(deleteButton);
+    //             item.append(item_text);
+    //             item.append(edit_input);
+    //             item.append(actionButton);
+    //             list.append(item);
+    
+    //             todoInput.value='';        
+    
+
+            
+
+    //         });
+
+          
+    // //    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  
